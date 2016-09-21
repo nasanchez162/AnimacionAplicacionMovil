c0276a44dd311b250a64f9ed80b625b38e1efa9b
nasanchez162 <na.sanchez162@uniandes.edu.co>
2016-09-21 10:24:49 -0500

    Primer commit

---
+++ app/src/main/java/com/aplicamoviles/mockupmov/Mapa.java
@@ -0,0 +1,35 @@
+package com.aplicamoviles.mockupmov;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Nelson on 20/09/2016.
+ */
+public class Mapa extends Activity {
+
+    Button b_retorno;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapa);
+
+        b_retorno = (Button) findViewById(R.id.b_retornar);
+    }
+
+    public void onClickMenuPrincipal(View view)
+    {
+        Intent i = new Intent(this, Menu_principal.class);
+        startActivity(i);
+
+    }
+
+}
+
---
+++ app/src/main/java/com/aplicamoviles/mockupmov/Mejoras.java
@@ -0,0 +1,30 @@
+package com.aplicamoviles.mockupmov;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Nelson on 20/09/2016.
+ */
+public class Mejoras extends Activity {
+
+    Button b_retorno;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapa);
+
+        b_retorno = (Button) findViewById(R.id.b_retornar);
+    }
+
+    public void onClickMenuPrincipal(View view)
+    {
+        Intent i = new Intent(this, Menu_principal.class);
+        startActivity(i);
+
+    }
+}
